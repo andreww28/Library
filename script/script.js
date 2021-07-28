ebb5eff28664a4341200e9a38ded702230985a96
John Andrew <johnandrewsanvictores@gmail.com>
2021-07-28 12:40:04 +0800

    update all

--- script/script.js
+++ script/script.js
@@ -20,6 +20,16 @@ let image_input = document.getElementById('image-input');
 let selected_img = document.getElementById('selected-img');
 let submit_form_btn = document.getElementById('submit-form-btn');
 
+let more_info_container = document.querySelector('.book-info-container');
+let plus_page_read_btn = document.querySelector('.page_read_plus_btn');
+let minus_page_read_btn = document.querySelector('.page_read_minus_btn');
+let info_total_page;
+let info_page_read;
+let info_page_read_field;
+let info_current_data;
+let info_current_index;
+
+let current_book_cover;
 let myLibrary;
 let selected_img_path;
 let book_img_file_name;
@@ -35,11 +45,17 @@ function main(){
     clear_input_field();
     retrieve_data();
     form_inputs_events();
+
     show_form_btn.addEventListener('click', show_form);
+    document.querySelector('.book-form #close-form-btn').addEventListener('click', hideForm.bind(event, form_container),false)
     page_switch.addEventListener('click', page_count_toggle);
     image_btn.addEventListener('click', () => image_input.click());
     image_input.addEventListener('change', image_input_event, false);
     submit_form_btn.addEventListener('click', submit_form_input_values);
+
+    document.querySelector('.book-info-container #close-form-btn').addEventListener('click', hideForm.bind(event, more_info_container),false);
+    plus_page_read_btn.addEventListener('click', set_info_progress_bar_status.bind(event, 'add'),false)
+    minus_page_read_btn.addEventListener('click', set_info_progress_bar_status.bind(event, 'minus'),false)
 }
 
 function Book(title,author,genre,total_page, page_read, book_img, img_file_name){
@@ -75,10 +91,9 @@ function show_form(){
     page_count_checkbox.checked = false;
 }
 
-function hideForm(){
+function hideForm(container, e){
     main_container.style.filter = "blur(0px)";
-    main_container.disabled = false;
-    form_container.style.top = '150vh';
+    container.style.top = '150vh';
 }
 
 
@@ -99,15 +114,19 @@ function image_input_event(e){
     selected_img.textContent = image_file;
     book_img_file_name = image_file;
 
-    
+    //crop_input_img(e.target.files[0]);
     var reader = new FileReader();  
-    reader.readAsDataURL(e.target.files[0])
+    reader.readAsDataURL(e.target.files[0]);
 
     reader.onload = function(e) { 
         var img_data_url = e.target.result;
         selected_img_path = img_data_url;
     };  
 }
+
+// function crop_input_img(img){
+    
+// }
 
 
 function form_inputs_events(){
@@ -249,20 +268,18 @@ function display_book(){
         create_book_element(parent = book_parent_tag, tag = 'p', _class = 'author', content = `<em>${book.author}</em>`);
         create_book_element(parent = book_parent_tag, tag = 'div', _class = 'progress-bar', content = '');
         create_book_element(parent = book_parent_tag, tag = 'p', _class = 'page-count', content = `Pages: ${book.page_read}/${book.total_page}`);
+        create_book_element(parent = book_parent_tag, tag = 'div', _class = 'more-info', content = 'i');
 
-        
-        let color_progress_bars = document.querySelectorAll('.progress-bar');
+        let color_progress_bars = document.querySelectorAll('.book > .progress-bar');
         let current_progress_bar = color_progress_bars[color_progress_bars.length - 1]; 
-        let width = (parseInt(book.page_read) / parseInt(book.total_page)) * 100;
-        current_progress_bar.style.setProperty('--progress-bar-width' , `${width}%`);
-
+        update_book_progress(book.page_read, book.total_page, current_progress_bar);
 
         let book_parents = document.querySelectorAll('.book');
         let book_covers = document.querySelectorAll('.book-cover');
         let titles = document.querySelectorAll(".book-title");
 
         let current_book_parent = book_parents[book_parents.length - 1];
-        let current_book_cover = book_covers[book_covers.length-1];        
+        current_book_cover = book_covers[book_covers.length-1];        
         let current_title = titles[titles.length - 1];
 
         let title_length = current_title.textContent.length;
@@ -284,6 +301,17 @@ function display_book(){
             current_title.textContent = cut_title;
         }
     });
+
+    let more_info_btns = Array.from(document.querySelectorAll('.more-info'));
+    let books = Array.from(document.querySelectorAll('.book'));
+
+    more_info_btns.forEach(btn => btn.addEventListener('click', show_book_info.bind(event, more_info_btns), false));
+    //books.forEach(book => book.addEventListener('click', show_book_info.bind(event, books), true));
+}
+
+function update_book_progress(page_read, total_page, progress_bar){
+    let width = (parseInt(page_read) / parseInt(total_page)) * 100;
+    progress_bar.style.setProperty('--progress-bar-width' , `${width}%`);
 }
 
 function create_book_element(parent, tag, _class, content, img_source){
@@ -297,6 +325,70 @@ function create_book_element(parent, tag, _class, content, img_source){
     }
 }
 
+function show_book_info(array, e){
+    main_container.style.filter = "blur(8px)";
+    more_info_container.setAttribute('style', 'top:calc((100vh - 45em)/2); transition: 1s;');
+
+    let index = array.indexOf(e.target);
+    let current_data = myLibrary[index];
+
+    let info_title = document.querySelector('#info-title');
+    let data_field = document.querySelectorAll('.info-value');
+    
+    more_info_container.style.backgroundImage = `linear-gradient(to bottom, rgba(5, 5, 5, 0.7), rgba(0, 0, 0, 0.9)), 
+    url('${current_data.book_img}')`;
+
+    info_title.textContent = current_data.title;
+    data_field[0].textContent = current_data.author;
+    data_field[1].textContent = current_data.genre;
+    data_field[2].textContent = current_data.total_page;
+    data_field[3].textContent = current_data.page_read;
+
+    info_total_page = parseInt(current_data.total_page);
+    info_page_read = parseInt(current_data.page_read);
+    info_page_read_field = data_field[3];
+    info_current_data = current_data;
+    info_current_index = index
+
+    set_info_progress_bar_status(operation = '');
+}
+
+function set_info_progress_bar_status(operation){
+    let book_page_count = document.querySelectorAll('.page-count')[info_current_index];
+    let info_progress_bar = document.querySelector('.info-progress-bar');
+    let info_status_value = document.querySelector('.info-status-value');
+    let page_read = info_page_read;
+    let total_page = info_total_page;
+
+    if(page_read >= parseInt(0) && page_read <=  total_page && operation != ''){
+        if(operation === 'add' && page_read < total_page){
+            page_read += 1;
+            info_page_read += 1;
+        }else if(operation === 'minus' && page_read > parseInt(0)){
+            page_read -= 1;
+            info_page_read -= 1;
+        }
+
+        info_page_read_field.textContent = page_read;
+        book_page_count.textContent = `Pages: ${page_read}/${total_page}`;
+        info_current_data['page_read'] = page_read;
+        add_data_to_LocalStorage();
+
+        let current_book_progress_bar = document.querySelectorAll('.book > .progress-bar')[info_current_index];
+        update_book_progress(info_current_data.page_read, info_current_data.total_page, current_book_progress_bar);
+    }
+
+    if(page_read === 0){
+        info_status_value.style.display = 'block';
+        info_progress_bar.style.display = 'none';
+    }else{
+        info_progress_bar.style.display = 'block';
+        info_status_value.style.display = 'none';
+
+        let width = (page_read / total_page) * 100;
+        info_progress_bar.style.setProperty('--info-progress-bar-width' , `${width}%`);
+    }
+}
 
 window.addEventListener('load', main, false);
 
